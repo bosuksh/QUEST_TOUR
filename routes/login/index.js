87af903fe063c802e4c724318f7817d3aa642945
redbird <cs20131575@u.sogang.ac.kr>
2018-06-25 02:09:41 +0900

    fixed error

--- routes/login/index.js
+++ routes/login/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const route = express.Router();
 
 module.exports = (app) => {
-    route.route('login')
+    route.route('/login')
         .get((req, res) => {
             res.render('login');
         })
